c658c7f526a4b5bee6ec07c6092bae814a946d8c
devendra kumar <devendrakumaraa@gmail.com>
2025-03-19 16:31:55 +0530

    Add Jobs and sample song api

---
+++ telegram_bot/api/jobs.js
@@ -0,0 +1,23 @@
+const { Axios } = require("../axios");
+
+async function jobsApi() {
+  const result = await Axios(
+    "GET",
+    "https://linkedin-jobs-api2.p.rapidapi.com/active-jb-24h",
+    {
+      "x-rapidapi-key": process.env.RAPID_API_KEY,
+      "x-rapidapi-host": process.env.JOBS_API_HOST,
+    },
+    {
+      title_filter: "Front end developer",
+      location_filter: "India",
+      type_filter: "FULL_TIME, INTERN",
+    }
+  );
+
+  return result;
+}
+
+module.exports = {
+  jobsApi,
+};
